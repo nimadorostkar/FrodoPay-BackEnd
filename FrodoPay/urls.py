1da8ae44f6cef263154f37d426f81178e7db61df
Fan Wang <nimadorostkar97@gmail.com>
2022-04-17 18:45:44 +0430

    wallet app

--- FrodoPay/urls.py
+++ FrodoPay/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('shop.urls')),
+    path('api/', include('wallet.urls')),
     path('api/', include('authentication.urls')),
 ]
 if settings.DEBUG:
