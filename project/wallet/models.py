3f77abe3facc5568403ba2e46e82ed3d371a33d4
Fan Wang <nimadorostkar97@gmail.com>
2022-04-21 12:51:40 +0430

    class Wallet models.Model

--- project/wallet/models.py
+++ project/wallet/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.dispatch import receiver
+from authentication.models import User
+import uuid
 
-# Create your models here.
+
+
+
+
+#------------------------------------------------------------------------------
+class Wallet(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  wallet_id = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+  inventory = models.DecimalField(max_digits=19, decimal_places=10)
+  date_created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+      return str(self.id)
