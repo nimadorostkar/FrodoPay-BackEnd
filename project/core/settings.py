7fe357c4ef0fad4d4ff91317fa1b9dd8895deaf9
Fan Wang <nimadorostkar97@gmail.com>
2022-04-21 12:19:48 +0430

    time zone and lang...

--- project/core/settings.py
+++ project/core/settings.py
@@ -145,16 +145,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tehran'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 
 
