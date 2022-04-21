298e7c3db4ed3dd0488622226f02f692da106bcb
Fan Wang <nimadorostkar97@gmail.com>
2022-04-21 14:05:42 +0430

    done

--- project/transactions/admin.py
+++ project/transactions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+
+
+
+#------------------------------------------------------------------------------
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('wallet', 'amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ['wallet__wallet_id']
+    raw_id_fields = ('wallet'),
+admin.site.register(Transaction, TransactionAdmin)
--- project/transactions/models.py
+++ project/transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.dispatch import receiver
+from wallet.models import Wallet
 
-# Create your models here.
+
+
+
+#------------------------------------------------------------------------------
+class Transaction(models.Model):
+    wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=30, decimal_places=5)
+    CHOICES = ( ('increase','increase'), ('decrease','decrease'), ('withdrawal','withdrawal') )
+    status = models.CharField(max_length=254, choices=CHOICES)
+    description = models.CharField(max_length=254, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status +"|"+ str(self.wallet.wallet_id)
