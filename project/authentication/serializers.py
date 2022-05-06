632fed23cf64ba603380b7b2b088e533cd27ac81
Fan Wang <nimadorostkar97@gmail.com>
2022-05-06 11:46:01 +0430

    Login done

--- project/authentication/serializers.py
+++ project/authentication/serializers.py
@@ -8,3 +8,12 @@ from .models import User
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=254, allow_null=False)
     password = serializers.CharField(max_length=254, allow_null=False)
+
+
+
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
--- project/authentication/views.py
+++ project/authentication/views.py
@@ -1,6 +1,6 @@
 from .models import User
 from django.http import JsonResponse
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, UsersSerializer
 from . import serializers
 from rest_framework.generics import GenericAPIView
 from rest_framework.decorators import api_view, permission_classes
@@ -8,21 +8,20 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework import viewsets, filters, status, pagination, mixins
 from rest_framework.response import Response
-
 from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
-
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+
 
 
 
 
 #------------------------------------------------------- Login ---------------
-class Login(GenericAPIView):
+class Login(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def post(self, request):
         serializer = serializers.LoginSerializer(data=request.data)
@@ -30,21 +29,16 @@ class Login(GenericAPIView):
             data = serializer.validated_data
         else:
             return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data = serializer.errors)
-        username = data['username']
-        password = data['password']
-        user = authenticate(request, username=username, password=password)
         try:
+            user = authenticate(request, username=data['username'], password=data['password'])
             login(request, user)
             token, created = Token.objects.get_or_create(user=user)
             print('API Auth Token: ', token.key)
             print('Created New Token:', created)
             user_data={"id":user.id, "first_name":user.first_name, "last_name":user.last_name, "image":user.photo.url, "token": token.key}
             return Response(user_data, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
-            return Response('کاربری با شماره {} یافت نشد، لطفا ثبت نام کنید'.format(data['username']) , status=status.HTTP_400_BAD_REQUEST)
-
-
-
+        except:
+            return Response('username or password is incorrect', status=status.HTTP_401_UNAUTHORIZED)
 
 
 
