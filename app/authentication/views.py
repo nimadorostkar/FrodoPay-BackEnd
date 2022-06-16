0da82e7ebece1707fadd5019b3a0c0e7c875677e
Fan Wang <nimadorostkar97@gmail.com>
2022-06-16 16:13:53 +0430

    ppp

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -283,7 +283,7 @@ class Countries(APIView):
 
 class Users(APIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
         users = models.User.objects.all()
@@ -307,7 +307,7 @@ class Users(APIView):
 
 class Usernames(APIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
         users = models.User.objects.all().values_list('username', flat=True)
