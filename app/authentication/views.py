03a6163bf7d710139f47901a3095c2a77c4674a6
Fan Wang <nimadorostkar97@gmail.com>
2022-07-11 16:31:48 +0430

    DataNotif debuged

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -456,19 +456,12 @@ class DataNotif(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        title = request.data['title']
-        body = request.data['body']
-        image = "url"
-        user = request.data['user']
         try:
-            device = FCMDevice.objects.filter(user=user)
-
-            device.send_message(Message(
-              #notification=Notification(title=title, body=body, image=image ),
-              data={ "username": "mohamad77777777", "body": "heeeeeeeeey" }
-            ))
-
+            user = models.User.objects.get(id=request.data['user'])
+            device = FCMDevice.objects.filter(user=request.data['user'])
+            device.send_message(Message( data={ "username":user.username, "title":request.data['title'], "body":request.data['body'], "type":request.data['type'] } ))
             return Response("data notification sent successfully" , status=status.HTTP_200_OK)
+
         except Exception as e:
             print(e)
             return Response("Error in sending data notification", status=status.HTTP_400_BAD_REQUEST)
