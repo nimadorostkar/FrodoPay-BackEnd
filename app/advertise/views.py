ecc16ff47399db7723b84bb146d3e9af8d1954cd
Fan Wang <nimadorostkar97@gmail.com>
2022-07-17 13:57:43 +0430

    Random Advertise APIView done

--- app/advertise/views.py
+++ app/advertise/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
-
+import random
 
 
 
@@ -27,8 +27,9 @@ class Advertise(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
-        ad = models.Advertise.objects.all()
-        serializer = AdvertiseSerializer(ad, many=True)
+        ads = list(models.Advertise.objects.all())
+        random_ad = random.choice(ads)
+        serializer = AdvertiseSerializer(random_ad)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
