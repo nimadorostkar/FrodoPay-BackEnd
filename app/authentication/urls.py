199c77725a93bec8a5985773b0b96e9aff6ea335
Fan Wang <nimadorostkar97@gmail.com>
2022-07-17 19:58:41 +0430

    login with otp done

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.views import TokenBlacklistView
 
 
 urlpatterns = [
-    path('login/', views.Login.as_view(), name='login'),
+    path('login_otp/', views.SendLoginOTP.as_view(), name='login_otp'),
+    path('login_confirmation/', views.LoginConf.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('register/', views.Register.as_view(), name='register'),
