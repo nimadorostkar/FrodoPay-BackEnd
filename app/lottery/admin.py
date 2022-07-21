18172c672d1b91f941fe3423ed1da5f201f98c16
Fan Wang <nimadorostkar97@gmail.com>
2022-07-21 08:32:58 +0430

    admin for lottery done

--- app/lottery/admin.py
+++ app/lottery/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Banner, UserScore, GetScore
 
-# Register your models here.
+
+
+
+
+#------------------------------------------------------------------------------
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('BannerImg','title')
+admin.site.register(Banner, BannerAdmin)
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class UserScoreAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score')
+    list_filter = ('user',)
+admin.site.register(UserScore, UserScoreAdmin)
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class GetScoreAdmin(admin.ModelAdmin):
+    list_display = ('invite', 'deposit', 'register')
+admin.site.register(GetScore, GetScoreAdmin)
+
+
+
+
+
+
+
+
+
+#End
