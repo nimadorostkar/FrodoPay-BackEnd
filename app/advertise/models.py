d922d3c85184e2b31f2c2a120634b4c707ecabcc
Fan Wang <nimadorostkar97@gmail.com>
2022-07-24 00:57:55 +0430

    HomeBanners

--- app/advertise/models.py
+++ app/advertise/models.py
@@ -19,3 +19,36 @@ class Advertise(models.Model):
 
     def __str__(self):
         return str(self.image)
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class HomeBanners(models.Model):
+    title = models.CharField(max_length=256, null=True, blank=True)
+    img = models.ImageField(upload_to='advertise/homebanner')
+
+    def BannerImg(self):
+        return format_html("<img width=30 src='{}'>".format(self.img.url))
+
+    def __str__(self):
+        return str(self.title)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#End
