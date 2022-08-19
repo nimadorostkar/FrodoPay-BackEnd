d1d0e488259a101fc67389fe3ea56aabf0cec393
Fan Wang <nimadorostkar97@gmail.com>
2022-08-19 21:33:34 +0430

    back to sqlite

--- app/core/settings.py
+++ app/core/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -127,7 +127,7 @@ DATABASES = {
         'PORT': 3306,
     }
 }
-
+'''
 
 
 
