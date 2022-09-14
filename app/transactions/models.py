e170e06aca8f94765dea1043065986998be84005
Fan Wang <nimadorostkar97@gmail.com>
2022-09-14 10:20:17 +0430

    p

--- app/transactions/models.py
+++ app/transactions/models.py
@@ -34,16 +34,6 @@ class Transaction(models.Model):
     def normalize_fee(self):
         return str(self.fee.normalize())
 
-    @receiver(post_save, sender=Payment)
-    def post_save(sender, instance, created, **kwargs):
-        if not created:
-            if instance.status == "PAID":
-                user = User.objects.get(email=instance.buyer_email)
-                deposit = Transaction(source="coinpayments", destination=user.username, type='deposit', status='success', amount=instance.amount_paid, description='deposit from coinpayments, payment:{}, provider_tx:{}'.format(instance.id,instance.provider_tx))
-                deposit.save()
-                user.inventory += instance.amount
-                user.save()
-
 
 
 
