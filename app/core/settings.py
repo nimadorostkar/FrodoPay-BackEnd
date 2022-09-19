38feca7c9028985b8e308e5f4da86b479a098fe6
Fan Wang <nimadorostkar97@gmail.com>
2022-09-19 12:29:11 +0430

    loggers in console

--- app/core/settings.py
+++ app/core/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
 
 
 
-
+'''
 LOGGING = {
 'version': 1,
 'disable_existing_loggers': False,
@@ -131,8 +131,22 @@ LOGGING = {
     },
 },
 }
-
-
+'''
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 
 
